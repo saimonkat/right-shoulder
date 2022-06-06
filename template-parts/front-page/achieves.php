ad986d4d758c1e81d10278ed77a6af7d8a7da7f2
Saimon Kat <saimon.kat23@gmail.com>
2022-06-06 10:06:55 +0300

    Achieves section

--- template-parts/front-page/achieves.php
+++ template-parts/front-page/achieves.php
@@ -1,5 +1,19 @@
 <section class="achieves section">
     <div class="container">
-        
+        <h2 class="visually-hidden">Наши достижения</h2>
+        <div class="achieves__list">
+            <div class="achieves__item achieve wow fadeInUp" data-wow-delay="">
+                <div class="achieve__title h2">20 лет</div>
+                <div class="achieve__text">Практикуем решение дел</div>
+            </div>
+            <div class="achieves__item achieve wow fadeInUp" data-wow-delay="100">
+                <div class="achieve__title h2">11 593 дел</div>
+                <div class="achieve__text">Успешно закрыто нашими специалистами</div>
+            </div>
+            <div class="achieves__item achieve wow fadeInUp" data-wow-delay="200">
+                <div class="achieve__title h2">761 444 000  ₽ </div>
+                <div class="achieve__text">Мы отсудили</div>
+            </div>
+        </div>
     </div>
 </section>
