9caefb9a1c51646a643f1949ef60ee6c60ee5fd0
Saimon Kat <saimon.kat23@gmail.com>
2022-06-08 23:29:16 +0300

    features hover update

--- template-parts/front-page/features.php
+++ template-parts/front-page/features.php
@@ -7,116 +7,46 @@
         <div class="features__slider">
             <div class="swiper-wrapper">
                 <div class="features__item feature swiper-slide">
-                    <div class="feature__inner">
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-1"></use>
-                            </svg>
-                            <div class="feature__title">Уголовные дела</div>
-                        </div>
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-1"></use>
-                            </svg>
-                            <div class="feature__title">Уголовные дела</div>
-                        </div>
-                    </div>
+                    <svg width="64" height="64" class="feature__icon">
+                        <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-1"></use>
+                    </svg>
+                    <div class="feature__title">Уголовные дела</div>
                 </div>
                 <div class="features__item feature swiper-slide">
-                    <div class="feature__inner">
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-2"></use>
-                            </svg>
-                            <div class="feature__title">Трудовые споры</div>
-                        </div>
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-2"></use>
-                            </svg>
-                            <div class="feature__title">Трудовые споры</div>
-                        </div>
-                    </div>
+                    <svg width="64" height="64" class="feature__icon">
+                        <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-2"></use>
+                    </svg>
+                    <div class="feature__title">Трудовые споры</div>
                 </div>
                 <div class="features__item feature swiper-slide">
-                    <div class="feature__inner">
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-3"></use>
-                            </svg>
-                            <div class="feature__title">Защита дольщиков</div>
-                        </div>
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-3"></use>
-                            </svg>
-                            <div class="feature__title">Защита дольщиков</div>
-                        </div>
-                    </div>
+                    <svg width="64" height="64" class="feature__icon">
+                        <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-3"></use>
+                    </svg>
+                    <div class="feature__title">Защита дольщиков</div>
                 </div>
                 <div class="features__item feature swiper-slide">
-                    <div class="feature__inner">
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-2"></use>
-                            </svg>
-                            <div class="feature__title">Наследственные дела</div>
-                        </div>
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-2"></use>
-                            </svg>
-                            <div class="feature__title">Наследственные дела</div>
-                        </div>
-                    </div>
+                    <svg width="64" height="64" class="feature__icon">
+                        <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-2"></use>
+                    </svg>
+                    <div class="feature__title">Наследственные дела</div>
                 </div>
                 <div class="features__item feature swiper-slide">
-                    <div class="feature__inner">
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-4"></use>
-                            </svg>
-                            <div class="feature__title">Семейные споры</div>
-                        </div>
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-4"></use>
-                            </svg>
-                            <div class="feature__title">Семейные споры</div>
-                        </div>
-                    </div>
+                    <svg width="64" height="64" class="feature__icon">
+                        <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-4"></use>
+                    </svg>
+                    <div class="feature__title">Семейные споры</div>
                 </div>
                 <div class="features__item feature swiper-slide">
-                    <div class="feature__inner">
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-5"></use>
-                            </svg>
-                            <div class="feature__title">Жилищные споры</div>
-                        </div>
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-5"></use>
-                            </svg>
-                            <div class="feature__title">Жилищные споры</div>
-                        </div>
-                    </div>
+                    <svg width="64" height="64" class="feature__icon">
+                        <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-5"></use>
+                    </svg>
+                    <div class="feature__title">Жилищные споры</div>
                 </div>
                 <div class="features__item feature swiper-slide">
-                    <div class="feature__inner">
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-3"></use>
-                            </svg>
-                            <div class="feature__title">Споры с банками</div>
-                        </div>
-                        <div class="feature__side">
-                            <svg width="64" height="64" class="feature__icon">
-                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-3"></use>
-                            </svg>
-                            <div class="feature__title">Споры с банками</div>
-                        </div>
-                    </div>
+                    <svg width="64" height="64" class="feature__icon">
+                        <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#feature-3"></use>
+                    </svg>
+                    <div class="feature__title">Споры с банками</div>
                 </div>
             </div>
             <div class="swiper-navigation">
