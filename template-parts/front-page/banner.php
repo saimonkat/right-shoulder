40f96874106152e9de76a87eb1cd899a2c1dc26d
Saimon Kat <saimon.kat23@gmail.com>
2022-06-08 09:30:07 +0300

    New banner section

--- template-parts/front-page/banner.php
+++ template-parts/front-page/banner.php
@@ -1,5 +1,8 @@
 <section class="banner section">
-    <div class="container">
-        
+    <div class="banner__container container">
+        <div class="banner__content">
+            <h2 class="banner__title">100% Гарантия результатов</h2>
+            <div class="banner__text">Текст описание для верстки например в три строки</div>
+        </div>
     </div>
 </section>
