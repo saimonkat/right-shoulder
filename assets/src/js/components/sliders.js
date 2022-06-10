86a1a685ac13b5612548d405e06946a1adf8cb31
Saimon Kat <saimon.kat23@gmail.com>
2022-06-10 11:52:20 +0300

    Blog slider

--- assets/src/js/components/sliders.js
+++ assets/src/js/components/sliders.js
@@ -2,31 +2,31 @@ import Swiper, { Navigation } from 'swiper';
 Swiper.use([Navigation]);
 
 export default function () {
-    const featuresSlider = document.querySelector('.features__slider');
-    featuresSlider && new Swiper(featuresSlider, {
-        direction: "horizontal",
-        slidesPerView: 'auto',
-        navigation: {
-            nextEl: '.swiper-button--next',
-            prevEl: '.swiper-button--prev',
-        },
-        spaceBetween: 10,
-        breakpoints: {
-            992: {
-                spaceBetween: 20
-            }
-        },
-    });
-    const reviewsSlider = document.querySelector('.reviews__slider');
+    const swipers = document.querySelectorAll('.swiper');
+    swipers && swipers.forEach(swiper => {
+        new Swiper(swiper, {
+            slidesPerView: 'auto',
+            navigation: {
+                nextEl: '.swiper-button--next',
+                prevEl: '.swiper-button--prev',
+            },
+            spaceBetween: 10,
+            breakpoints: {
+                768: {
+                    spaceBetween: 20
+                }
+            },
+        });
+    })
 
+    const reviewsSlider = document.querySelector('.reviews__slider');
     reviewsSlider && new Swiper(reviewsSlider, {
-        direction: "horizontal",
         slidesPerView: 'auto',
-        spaceBetween: 20,
         navigation: {
             nextEl: '.swiper-button--next',
             prevEl: '.swiper-button--prev',
         },
+        spaceBetween: 20,
         breakpoints: {
             992: {
                 spaceBetween: 52
