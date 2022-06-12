6f90ed42dafc2841320a20102654b79a994b9cf3
Saimon Kat <saimon.kat23@gmail.com>
2022-06-12 16:45:47 +0300

    vh-fix mobile disable

--- assets/src/js/fixes/vh-fix.js
+++ assets/src/js/fixes/vh-fix.js
@@ -2,7 +2,4 @@ const appHeight = () => {
     document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
 }
 window.addEventListener('DOMContentLoaded', appHeight)
-window.addEventListener('resize', appHeight)
 
-const mobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-mobileDevice && window.addEventListener('scroll', appHeight)
