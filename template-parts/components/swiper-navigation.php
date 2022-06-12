ea25ab029ba128f586bbf58681956521fa3c153c
Saimon Kat <saimon.kat23@gmail.com>
2022-06-12 18:00:35 +0300

    Sliders updates

--- template-parts/components/swiper-navigation.php
+++ template-parts/components/swiper-navigation.php
@@ -1,11 +1,11 @@
 <div class="swiper-navigation">
     <div class="swiper-button swiper-button--prev">
-        <svg width="48" height="48">
+        <svg width="18" height="16">
             <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#arrow"></use>
         </svg>
     </div>
     <div class="swiper-button swiper-button--next">
-        <svg width="48" height="48">
+        <svg width="18" height="16">
             <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#arrow"></use>
         </svg>
     </div>
