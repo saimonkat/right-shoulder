73cd3343a460e171cac715c3ee976a206cd2d8c2
Saimon Kat <saimon.kat23@gmail.com>
2022-06-12 20:20:46 +0300

    Reviews videos

--- template-parts/front-page/reviews.php
+++ template-parts/front-page/reviews.php
@@ -7,19 +7,20 @@
         <div class="reviews__slider swiper">
             <div class="swiper-wrapper">
                 <div class="reviews__item review swiper-slide">
-                    <div class="review__video"
-                         data-fancybox
-                         data-src="https://www.youtube.com/watch?v=BKPStyM2suU">
-                        <picture class="review__video-bg">
-                            <source srcset="<?= DIST_URI ?>/img/base/review.webp"
-                                type="image/webp">
-                            <img src="<?= DIST_URI ?>/img/base/review.jpg"
-                                data-src="<?= DIST_URI ?>/img/base/review.jpg"
-                                alt="">
-                        </picture>
-                        <svg width="20" height="27">
-                            <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#play"></use>
-                        </svg>
+                    <div class="review__video">
+                        <div class="review__video-bg">
+                            <picture class="review__video-poster">
+                                <source srcset="<?= DIST_URI ?>/img/base/review.webp"
+                                    type="image/webp">
+                                <img src="<?= DIST_URI ?>/img/base/review.jpg"
+                                    data-src="<?= DIST_URI ?>/img/base/review.jpg"
+                                    alt="">
+                            </picture>
+                            <svg width="20" height="27">
+                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#play"></use>
+                            </svg>
+                        </div>
+                        <video src="<?= DIST_URI ?>/img/base/review.mp4"></video>
                     </div>
                     <div class="review__content">
                         <div class="review__name">
@@ -34,19 +35,20 @@
                     </div>
                 </div>
                 <div class="reviews__item review swiper-slide">
-                    <div class="review__video"
-                         data-fancybox
-                         data-src="https://www.youtube.com/watch?v=BKPStyM2suU">
-                        <picture class="review__video-bg">
-                            <source srcset="<?= DIST_URI ?>/img/base/review.webp"
-                                type="image/webp">
-                            <img src="<?= DIST_URI ?>/img/base/review.jpg"
-                                data-src="<?= DIST_URI ?>/img/base/review.jpg"
-                                alt="">
-                        </picture>
-                        <svg width="20" height="27">
-                            <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#play"></use>
-                        </svg>
+                    <div class="review__video">
+                        <div class="review__video-bg">
+                            <picture class="review__video-poster">
+                                <source srcset="<?= DIST_URI ?>/img/base/review.webp"
+                                    type="image/webp">
+                                <img src="<?= DIST_URI ?>/img/base/review.jpg"
+                                    data-src="<?= DIST_URI ?>/img/base/review.jpg"
+                                    alt="">
+                            </picture>
+                            <svg width="20" height="27">
+                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#play"></use>
+                            </svg>
+                        </div>
+                        <video src="<?= DIST_URI ?>/img/base/review.mp4"></video>
                     </div>
                     <div class="review__content">
                         <div class="review__name">
@@ -61,19 +63,20 @@
                     </div>
                 </div>
                 <div class="reviews__item review swiper-slide">
-                    <div class="review__video"
-                         data-fancybox
-                         data-src="https://www.youtube.com/watch?v=BKPStyM2suU">
-                        <picture class="review__video-bg">
-                            <source srcset="<?= DIST_URI ?>/img/base/review.webp"
-                                type="image/webp">
-                            <img src="<?= DIST_URI ?>/img/base/review.jpg"
-                                data-src="<?= DIST_URI ?>/img/base/review.jpg"
-                                alt="">
-                        </picture>
-                        <svg width="20" height="27">
-                            <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#play"></use>
-                        </svg>
+                    <div class="review__video">
+                        <div class="review__video-bg">
+                            <picture class="review__video-poster">
+                                <source srcset="<?= DIST_URI ?>/img/base/review.webp"
+                                    type="image/webp">
+                                <img src="<?= DIST_URI ?>/img/base/review.jpg"
+                                    data-src="<?= DIST_URI ?>/img/base/review.jpg"
+                                    alt="">
+                            </picture>
+                            <svg width="20" height="27">
+                                <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#play"></use>
+                            </svg>
+                        </div>
+                        <video src="<?= DIST_URI ?>/img/base/review.mp4"></video>
                     </div>
                     <div class="review__content">
                         <div class="review__name">
