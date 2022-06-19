713c9b9acd6b8b87c0a791577d3fb571ab96f23d
abies-bracteata <102981907+abies-bracteata@users.noreply.github.com>
2022-06-19 12:23:29 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -49,3 +49,26 @@ add_filter( 'autoptimize_filter_imgopt_lazyload_cssoutput', 'nonoscript_lazyload
 
 require get_template_directory() . '/app/vendor/autoload.php';
 require get_template_directory() . '/app/init.php';
+
+
+add_action( 'wp_footer', 'metrika' );
+
+function metrika(){
+	?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+       m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+       ym(89240116, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+       });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/89240116" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+	<?php
+}
