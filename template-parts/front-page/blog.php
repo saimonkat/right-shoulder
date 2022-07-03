cab7e2482db929f3e74c8b2b416822ac478876bf
abies-bracteata <102981907+abies-bracteata@users.noreply.github.com>
2022-07-03 15:43:35 +0300

    New

--- template-parts/front-page/blog.php
+++ template-parts/front-page/blog.php
@@ -5,96 +5,36 @@
         </h2>
         <div class="blog__slider swiper">
             <div class="swiper-wrapper">
-                <div class="blog__item blog-post swiper-slide">
-                    <picture class="blog-post__img">
-                        <source srcset="<?= DIST_URI ?>/img/base/blog-post.webp"
-                            type="image/webp">
-                        <img src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            data-src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            alt="">
-                    </picture>
-                    <div class="blog-post__content">
-                        <div class="blog-post__title h3">
-                            Перечень документов, необходимых для процедуры банкротства физического лица
-                        </div>
-                        <div class="blog-post__text">
-                            Текст описание для верстки<br> например в три строки
-                        </div>
-                        <a href="#" class="blog-post__btn btn">Читать</a>
-                    </div>
-                </div>
-                <div class="blog__item blog-post swiper-slide">
-                    <picture class="blog-post__img">
-                        <source srcset="<?= DIST_URI ?>/img/base/blog-post.webp"
-                            type="image/webp">
-                        <img src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            data-src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            alt="">
-                    </picture>
-                    <div class="blog-post__content">
-                        <div class="blog-post__title h3">
-                            Перечень документов, необходимых для процедуры банкротства физического лица
-                        </div>
-                        <div class="blog-post__text">
-                            Текст описание для верстки<br> например в три строки
-                        </div>
-                        <a href="#" class="blog-post__btn btn">Читать</a>
-                    </div>
-                </div>
-                <div class="blog__item blog-post swiper-slide">
-                    <picture class="blog-post__img">
-                        <source srcset="<?= DIST_URI ?>/img/base/blog-post.webp"
-                            type="image/webp">
-                        <img src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            data-src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            alt="">
-                    </picture>
-                    <div class="blog-post__content">
-                        <div class="blog-post__title h3">
-                            Перечень документов, необходимых для процедуры банкротства физического лица
-                        </div>
-                        <div class="blog-post__text">
-                            Текст описание для верстки<br> например в три строки
-                        </div>
-                        <a href="#" class="blog-post__btn btn">Читать</a>
-                    </div>
-                </div>
-                <div class="blog__item blog-post swiper-slide">
-                    <picture class="blog-post__img">
-                        <source srcset="<?= DIST_URI ?>/img/base/blog-post.webp"
-                            type="image/webp">
-                        <img src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            data-src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            alt="">
-                    </picture>
-                    <div class="blog-post__content">
-                        <div class="blog-post__title h3">
-                            Перечень документов, необходимых для процедуры банкротства физического лица
-                        </div>
-                        <div class="blog-post__text">
-                            Текст описание для верстки<br> например в три строки
-                        </div>
-                        <a href="#" class="blog-post__btn btn">Читать</a>
-                    </div>
-                </div>
-                <div class="blog__item blog-post swiper-slide">
-                    <picture class="blog-post__img">
-                        <source srcset="<?= DIST_URI ?>/img/base/blog-post.webp"
-                            type="image/webp">
-                        <img src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            data-src="<?= DIST_URI ?>/img/base/blog-post.jpg"
-                            alt="">
-                    </picture>
-                    <div class="blog-post__content">
-                        <div class="blog-post__title h3">
-                            Перечень документов, необходимых для процедуры банкротства физического лица
-                        </div>
-                        <div class="blog-post__text">
-                            Текст описание для верстки<br> например в три строки
-                        </div>
-                        <a href="#" class="blog-post__btn btn">Читать</a>
-                    </div>
-                </div>
+
+            <?php
+
+			$args = array(
+				'post_type' 	 => 'post',
+				'posts_per_page' => 6,
+				'publish' 		 => true
+			);
+
+
+			$q = new WP_Query($args);
+
+            if( $q->have_posts() ) :
+
+                while( $q->have_posts() ): $q->the_post();
+
+                get_template_part('template-parts/excerpt/excerpt-post');
+
+                endwhile;
+
+			else:
+
+			echo 'Здесь пока ничего нет';
+
+            endif;
+
+			wp_reset_postdata();
+
+            ?>
+
             </div>
             <?php get_template_part('template-parts/components/swiper-navigation'); ?>
         </div>
