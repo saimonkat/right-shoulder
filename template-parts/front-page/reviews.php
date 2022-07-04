3198f78a101b3dceafbe00df8e8195afd2f66ec0
Saimon Kat <saimon.kat23@gmail.com>
2022-07-04 17:55:41 +0300

    Reviews section ACF fix

--- template-parts/front-page/reviews.php
+++ template-parts/front-page/reviews.php
@@ -11,35 +11,34 @@
                 <?php foreach($reviews['list'] as $review): ?>
                     
                     <div class="reviews__item review swiper-slide">
+                        <?php if ($review['preview']): ?>
                         <div class="review__video">
                             <div class="review__video-bg">
                                 <picture class="review__video-poster">
-                                    <!-- <source srcset="<?= $review['preview'] ?>"
-                                        type="image/webp"> -->
-                                        <?php if ($achieve['preview']): ?>
-                                            <img src="<?= $review['preview'] ?>"
-                                            data-src="<?= $review['preview'] ?>"
-                                            alt="">
-                                        <?php endif; ?>
-                                    </picture>
-                                    <svg width="20" height="27">
-                                        <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#play"></use>
-                                    </svg>
-                                </div>
+                                    <!-- <source srcset="<?= $review['preview'] ?>" type="image/webp"> -->
+                                    <img src="<?= $review['preview'] ?>"
+                                         data-src="<?= $review['preview'] ?>"
+                                         alt="">
+                                </picture>
+                                <svg width="20" height="27">
+                                    <use xlink:href="<?= DIST_URI ?>/img/icons/svg-sprite.svg#play"></use>
+                                </svg>
                                 <video src="<?= $review['video']['url'] ?>"></video>
                             </div>
-                            <div class="review__content">
-                                <div class="review__name">
-                                    <?= $review['name'] ?>
-                                </div>
-                                <div class="review__type">
-                                    <?= $review['type'] ?>
-                                </div>
-                                <div class="review__text">
-                                    <?= $review['text'] ?>
-                                </div>
+                        </div>
+                        <?php endif; ?>
+                        <div class="review__content">
+                            <div class="review__name">
+                                <?= $review['name'] ?>
+                            </div>
+                            <div class="review__type">
+                                <?= $review['type'] ?>
+                            </div>
+                            <div class="review__text">
+                                <?= $review['text'] ?>
                             </div>
                         </div>
+                    </div>
                     <?php endforeach; ?>
                 </div>
                 <?php get_template_part('template-parts/components/swiper-navigation'); ?>
