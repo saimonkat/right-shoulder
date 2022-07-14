babf4eeef595831d38bfef3284c87a9d60c97a16
Saimon Kat <saimon.kat23@gmail.com>
2022-07-14 03:27:15 +0300

    Anchor scroll fixes

--- assets/src/js/global/anchor-scroll.js
+++ assets/src/js/global/anchor-scroll.js
@@ -1,17 +1,19 @@
 export default function(){
     location.hash && smoothScrollTo(location.hash);
-    const anchors = document.querySelectorAll('a[href^="#"]');
+    const anchors = document.querySelectorAll('a[href^="#"], a[href^="/#"]');
 
     anchors.length && anchors.forEach(anchor => {
         anchor.addEventListener("click", function(e){
-            e.preventDefault();
-            smoothScrollTo(anchor.getAttribute('href'));
+            smoothScrollTo(anchor.getAttribute('href').slice(1), e);
         });
     })
 
-    function smoothScrollTo(target){
+    function smoothScrollTo(target, e){
+        target = target.replaceAll('/', '');
         const targetEl = document.querySelector(target);
+
         if (targetEl) {
+            e && e.preventDefault();
             const offsetTop = document.querySelector(target).offsetTop;
 
             scroll({
